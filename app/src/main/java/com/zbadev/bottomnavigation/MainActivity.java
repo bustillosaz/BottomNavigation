190644b2b003021d9f463dcc79193edd973064b0
bustillosaz <zadibustillosalbornoz@gmail.com>
2024-06-05 11:33:15 -0500

    commit slider menu

--- app/src/main/java/com/zbadev/bottomnavigation/MainActivity.java
+++ app/src/main/java/com/zbadev/bottomnavigation/MainActivity.java
@@ -1,40 +1,85 @@
 package com.zbadev.bottomnavigation;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.annotation.NonNull;
+import androidx.core.view.GravityCompat;
 
 import android.app.Dialog;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.widget.ImageView;
+import android.view.MenuItem;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
+//import android.widget.Toolbar;
+import android.widget.ShareActionProvider;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.internal.NavigationMenu;
+import com.google.android.material.navigation.NavigationView;
 import com.zbadev.bottomnavigation.databinding.ActivityMainBinding;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
-    ActivityMainBinding binding;
+    //ActivityMainBinding binding;
     public static int id=0x7f030004;
+    public static int ids=0x7f030004;
+    //Para menu slider
+    FloatingActionButton fab;
+    DrawerLayout drawerLayout;
+    BottomNavigationView bottomNavigationView;
+
+    FragmentManager fragmentManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-        //setContentView(R.layout.activity_main);
+        /*binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());*/
+        setContentView(R.layout.activity_main);
 
+
+        //Para menu slider
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        fab = findViewById(R.id.fab);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        //drawerLayout =(DrawerLayout) getLayoutInflater().inflate(R.layout.activity_main,null);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+
+        // Asegurarse de que la Toolbar se infló correctamente
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_nav, R.string.close_nav);
+
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout, new HomeFragment()).commit();
+            navigationView.setCheckedItem(R.id.nav_home);
+        }
+        // del otro
         replaceFragment(new HomeFragment());
-        binding.bottomNavigationView.setBackground(null);
+        bottomNavigationView.setBackground(null);
 
-        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+        bottomNavigationView.setOnItemSelectedListener(item -> {
             id = item.getItemId();
             if(id == R.id.home){
                 replaceFragment(new HomeFragment());
@@ -47,13 +92,15 @@ public class MainActivity extends AppCompatActivity {
             }
             return true;
         });
-        binding.fab.setOnClickListener(new View.OnClickListener() {
+        fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 showBottomDialog();
             }
         });
     }
+
+
     private void replaceFragment(Fragment fragment){
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
@@ -85,7 +132,6 @@ public class MainActivity extends AppCompatActivity {
         shortsLayout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 dialog.dismiss();
                 Toast.makeText(MainActivity.this,"Create a short is Clicked",Toast.LENGTH_SHORT).show();
 
@@ -114,6 +160,59 @@ public class MainActivity extends AppCompatActivity {
         dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
         dialog.getWindow().getAttributes().windowAnimations = R.style.DialogAnimation;
         dialog.getWindow().setGravity(Gravity.BOTTOM);
-
     }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        try {
+            ids = item.getItemId();
+            if(ids == R.id.nav_home){
+                getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout, new HomeFragment()).commit();
+                return true;
+            } else if (ids == R.id.nav_settings) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout, new ShortsFragment()).commit();
+                return true;
+            } else if (ids == R.id.nav_share) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout, new SuscriptionFragment()).commit();
+            }else if (ids == R.id.nav_about){
+                getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout, new LibraryFragment()).commit();
+            } else if (ids == R.id.nav_logout) {
+                Toast.makeText(this, "Logout!", Toast.LENGTH_SHORT).show();
+            }
+
+        /*switch (item.getItemId()) {
+            case R.id.nav_home:
+                getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout, new HomeFragment()).commit();
+                break;
+            case R.id.nav_settings:
+                getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout, new ShortsFragment()).commit();
+                break;
+            case R.id.nav_share:
+                getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout, new SuscriptionFragment()).commit();
+                break;
+            case R.id.nav_about:
+                getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout, new LibraryFragment()).commit();
+                break;
+            case R.id.nav_logout:
+                Toast.makeText(this, "Logout!", Toast.LENGTH_SHORT).show();
+                break;
+        }*/
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }catch (Exception e){
+            Toast.makeText(this,"Data not inserted" + e.getMessage(),Toast.LENGTH_LONG).show();
+        }
+
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
 }
